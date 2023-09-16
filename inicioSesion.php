7733695b40e6f216f2dc9bab9c88f8c554b36d47
KEVINb SB <U21214433@utp.edu.pe>
2023-09-16 14:47:19 -0500

    cambio

--- inicioSesion.php
+++ inicioSesion.php
@@ -6,11 +6,8 @@
     <link rel="stylesheet" href="./css/inicioSesion.css">
     <title>Document</title>
 </head>
-<body>    
-    <picture class="astro-imagetools-picture astro-imagetools-picture-5E5E8C44" style="position: relative; display: inline-block; width: 100%; height: 100%; --z-index: 1; --opacity: 0;">
-    <source srcset="https://cfcdn.apowersoft.info/astro/picwish/_astro/background-hero@3840w.651dfd6a.avif" sizes="(min-width: 3840px) 3840px, 100vw" width="3840" height="2160" type="image/avif">
-    <img src="https://cfcdn.apowersoft.info/astro/picwish/_astro/background-hero@3840w.3195571" alt=" " srcset="https://cfcdn.apowersoft.info/astro/picwish/_astro/background-hero@3840w.3195571c.jpeg" sizes="(min-width: 3840px) 3840px, 100vw" width="3840" height="2160" loading="lazy" decoding="async" class="astro-imagetools-img" style="display: inline-block; overflow: hidden; vertical-align: middle; ; width: 100%; height: 100%;" onload="parentElement.style.setProperty('--z-index', 1); parentElement.style.setProperty('--opacity', 0);">
-    </picture>
+<body >
+    <img class="fondo" src="./img/iniciosesion.jpg" alt="">
     <div>
         iniciar sesion
     </div>
