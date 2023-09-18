b7745a717086ae7ee036295f157996adb468cbb3
KEVINb SB <U21214433@utp.edu.pe>
2023-09-17 22:17:38 -0500

    cambio

--- inicioSesion.php
+++ inicioSesion.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/inicioSesion.css">
     <title>Document</title>
 </head>
-<body >
-    <img class="fondo" src="./img/iniciosesion.jpg" alt="">
-    <div>
-        iniciar sesion
+
+<body>
+    <div class="container">
+        <div class="info">
+            <p class="txt1">
+                ¡Gracias por visitarnos!
+            </p>
+            <h2>
+                Reserva ya iniciando sesión!
+            </h2>
+            <hr>
+            <p class="txt2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quaerat architecto molestiae in itaque
+                fugit porro repellat ad ut atque incidunt, praesentium modi dolor perferendis optio veritatis voluptates
+                possimus. Molestiae.
+            </p>
+        </div>
+
+        <form class="sesion" action="post">
+            <h2>Inicia Sesión</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt repudiandae, quo quas vitae possimus
+                eaque necessitatibus illo ipsam aliquid. Quam officiis atque vitae adipisci nostrum asperiores quia
+                aliquam sed similique.
+            </p>
+            <div class="datos">
+                <input type="email" class="box" placeholder="Ingresa tu correo">
+                <input type="password" class="box" placeholder="Ingresa tu contraseña">
+
+                <a href="#">¿Olvidaste tu contraseña?</a>
+                
+                <input type="submit" value="login" class="submit">
+            </div>
+        </form>
     </div>
 </body>
+
 </html>
